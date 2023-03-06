fa298a4eb34cb6cdb29441bffc968a3f45658cdc
Marnie Reid <marnie@kodewithklossy.com>
2023-03-06 11:40:25 +0100

    Initial Commit

---
+++ Account Creation/Account_CreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Account_CreationApp.swift
+//  Account Creation
+//
+//  Created by scholar on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Account_CreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
